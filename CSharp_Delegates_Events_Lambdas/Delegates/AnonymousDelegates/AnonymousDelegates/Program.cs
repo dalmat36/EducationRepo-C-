43c4356b7c5f7d6a761ecf3715d7f103cd69b1ad
Matt.Dalesio <matt.dalesio@accuweather.com>
2018-02-22 13:33:33 -0500

    Completed using anonymous delegates

--- CSharp_Delegates_Events_Lambdas/Delegates/AnonymousDelegates/AnonymousDelegates/Program.cs
+++ CSharp_Delegates_Events_Lambdas/Delegates/AnonymousDelegates/AnonymousDelegates/Program.cs
@@ -13,7 +13,10 @@ namespace AnonymousDelegates
     {
         static void Main(string[] args)
         {
-            // SNIPPET GOES HERE
+            MyDelegate f = delegate (int arg1, int arg2) {
+                return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine("The number is: " + f(10, 20));
 
             // Keep the console window open until a key is pressed
             Console.WriteLine("\nPress Enter Key to Continue...");
